1a771506b5cff706ca9f97da538ce8df2322d5b2
Pugmaster Flash <michael.sarr@gmail.com>
2023-09-11 00:03:36 -0700

    working primg line and circle

--- src/vga/sys/primg.c
+++ src/vga/sys/primg.c
@@ -1,19 +1,25 @@
 
 #include "primg.h"
-
+uint16_t primg_x0;
+uint16_t primg_y0;
+uint8_t primg_color;
 
 uint16_t get_unit16(uint8_t * in)
 {
     uint16_t x = (*(in+1) *  256) | *in;
     return x;
 }
+
+
 void vga_drawline(uint16_t struct_address)
 { 
-primg_x0 = get_unit16(xram+struct_address);
-uint16_t x1 = get_unit16(xram+struct_address+2); 
-primg_y0 = get_unit16(xram+struct_address+4); 
-uint16_t y1 = get_unit16(xram+struct_address+6); 
+primg_x0 = *((uint16_t*)(xram+struct_address));
+primg_y0 = *((uint16_t*)(xram+struct_address+2));
+uint16_t x1 = *((uint16_t*)(xram+struct_address+4));
+uint16_t y1 = *((uint16_t*)(xram+struct_address+6));
+
 primg_color =  xram[struct_address+8];
+//printf("server side drawline(%d,%d,%d,%d,%d)\n",primg_x0,primg_y0,x1,y1,primg_color);
 /*
 i could not seem to figure out how to cast the address coming in as an int to a pointer address to my structure type
 everything I seemed to try came in with some other address , not something im familar with any suggestions would be apprciated
@@ -37,32 +43,56 @@ int16_t err = dx + dy, e2; /* error value e_xy */
   }
 }// end drawline
  
-void setPixel() 
+void setPixel(uint16_t x, uint16_t y,uint8_t color) 
 {
+ // printf("%d, %d ,%d \n",primg_x0,primg_y0,primg_color);
     // insert primg_color @ primg_x0, primg_y0
-    uint16_t offset = (primg_x0 / 2) + (primg_y0 * (320 / 2));
-    if (primg_x0 % 2)
-     xram[offset] = (xram[offset] & 15) | (primg_color << 4);
+    uint16_t offset = (x / 2) + (y * (320 / 2));
+    if (x % 2)
+     xram[offset] = (xram[offset] & 15) | (color << 4);
     else
-      xram[offset] = (xram[offset] & 240) | primg_color;
+      xram[offset] = (xram[offset] & 240) | color;
 }
 
 
 void plot_circle (uint16_t struct_address)
 {
-primg_x0 = get_unit16(xram+struct_address);
-primg_y0 = get_unit16(xram+struct_address+2);
-uint16_t r   = get_unit16(xram+struct_address+4);
-primg_color =  xram[struct_address+6];
-
-   int16_t x = -r, y = 0, err = 2-2*r; /* II. Quadrant */ 
-   do {
-      setPixel (primg_x0-x, primg_y0+y,primg_color); /*   I. Quadrant */
-      setPixel (primg_x0-y, primg_y0-x,primg_color); /*  II. Quadrant */
-      setPixel (primg_x0+x, primg_y0-y,primg_color); /* III. Quadrant */
-      setPixel (primg_x0+y, primg_y0+x,primg_color); /*  IV. Quadrant */
-      r = err;
-      if (r >  x) err += ++x*2+1; /* e_xy+e_x > 0 */
-      if (r <= y) err += ++y*2+1; /* e_xy+e_y < 0 */
-   } while (x < 0);
+ uint16_t x0= *((uint16_t*)(xram+struct_address));
+ uint16_t y0 = *((uint16_t*)(xram+struct_address+2));
+ uint16_t radius = *((uint16_t*)(xram+struct_address+4));
+ uint16_t color =  xram[struct_address+6];
+ 
+ int16_t f = 1 - radius;
+  int16_t ddF_x = 1;
+  int16_t ddF_y = -2 * radius;
+  int16_t x = 0;
+  int16_t y = radius;
+ 
+  setPixel (x0, y0 + radius,color);
+  setPixel (x0, y0 - radius,color);
+  setPixel (x0 + radius, y0,color);
+  setPixel (x0 - radius, y0,color);
+  while (x < y)
+  {
+    // ddF_x == 2 * x + 1;
+    // ddF_y == -2 * y;
+    // f == x*x + y*y - radius*radius + 2*x - y + 1;
+    if (f >= 0) 
+    {
+      y--;
+      ddF_y += 2;
+      f += ddF_y;
+    }
+    x++;
+    ddF_x += 2;
+    f += ddF_x;    
+    setPixel (x0 + x, y0 + y,color);
+    setPixel (x0 - x, y0 + y,color);
+    setPixel (x0 + x, y0 - y,color);
+    setPixel (x0 - x, y0 - y,color);
+    setPixel (x0 + y, y0 + x,color);
+    setPixel (x0 - y, y0 + x,color);
+    setPixel (x0 + y, y0 - x,color);
+    setPixel (x0 - y, y0 - x,color);
+  }
 }
--- src/vga/sys/primg.h
+++ src/vga/sys/primg.h
@@ -7,14 +7,15 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "xram.h"
-uint16_t primg_x0;
-uint16_t primg_y0;
-uint8_t primg_color;
+
+#include <stdio.h>
+
+
 
 uint16_t get_unit16(uint8_t * in);
 
 
-void setPixel();
+void setPixel(uint16_t x, uint16_t y,uint8_t color);
 void vga_drawline(uint16_t struct_address);
 void plot_circle (uint16_t struct_address);
 
